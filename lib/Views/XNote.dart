455f52e03de9ad688df5ff56e773ec3cdd412fa7
FSOFT.FPT.VN\HaiPM1 <haipm1@fsoft.com.vn>
2019-08-11 15:13:16 +0700

    first screen

--- lib/Views/XNote.dart
+++ lib/Views/XNote.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
-import 'dart:math' as math;
+//import 'package:flutter/foundation.dart';
+//import 'dart:math' as math;
 import '../ClientModel.dart';
 import '../DBIf.dart';
 import 'NoteDetail.dart';
@@ -66,40 +66,114 @@ class XNoteState extends State<XNote> {
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("X-Note")),
-      body: FutureBuilder<List<NoteItem>>(
-        future: DBProvider.db.getAllClients(),
-        builder: (BuildContext context, AsyncSnapshot<List<NoteItem>> snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (BuildContext context, int index) {
-                NoteItem item = snapshot.data[index];
-                return Dismissible(
-                  key: UniqueKey(),
-                  background: Container(color: Colors.red),
-                  onDismissed: (direction) {
-                    DBProvider.db.deleteClient(item.id);
-                  },
-                  //child: NoteItemView.viewTitle(item, this),
-                  child: NoteItemView(item, this),
-                );
-              },
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () async {
-          NoteItem rnd = testClients[math.Random().nextInt(testClients.length)];
-          await DBProvider.db.newClient(rnd);
-          setState(() {});
-        },
-      ),
+    return MaterialApp(
+        home: DefaultTabController(
+          length: 5,
+          child: Scaffold(
+            appBar: AppBar(
+              title: Text("X-Note"),
+              backgroundColor: Colors.blue,
+
+              bottom: TabBar(
+                labelPadding: EdgeInsetsDirectional.only(),
+                  tabs: [
+                    Container(
+                      width: 300.0,
+                      height: 50.0,
+                      color: Colors.green,
+                      padding: EdgeInsetsDirectional.only(),
+                      child:  FittedBox(
+                        child: Text("All"),
+                        fit: BoxFit.scaleDown
+                      )
+                    ),
+                    Container(
+                        width: 300.0,
+                        height: 50.0,
+                        color: Colors.amber,
+                        padding: EdgeInsetsDirectional.only(),
+                        child:  FittedBox(
+                            child: Text("Life"),
+                            fit: BoxFit.scaleDown
+                        )
+                    ),
+                    Container(
+                        width: 300.0,
+                        height: 50.0,
+                        color: Colors.red,
+                        padding: EdgeInsetsDirectional.only(),
+                        child:  FittedBox(
+                            child: Text("Work"),
+                            fit: BoxFit.scaleDown
+                        )
+                    ),
+                    Container(
+                        width: 300.0,
+                        height: 50.0,
+                        color: Colors.orange,
+                        padding: EdgeInsetsDirectional.only(),
+                        child:  FittedBox(
+                            child: Text("Fin"),
+                            fit: BoxFit.scaleDown
+                        )
+                    ),
+                    Container(
+                        width: 300.0,
+                        height: 50.0,
+                        color: Colors.deepPurple,
+                        padding: EdgeInsetsDirectional.only(),
+                        child:  FittedBox(
+                            child: Text("Other"),
+                            fit: BoxFit.scaleDown
+                        )
+                    ),
+                  ],
+
+              ),
+            ),
+            body: TabBarView(
+              children: [
+                Icon(Icons.directions_car),
+                FutureBuilder<List<NoteItem>>(
+                  future: DBProvider.db.getAllClients(),
+
+                  builder: (BuildContext context, AsyncSnapshot<List<NoteItem>> snapshot) {
+                    if (snapshot.hasData) {
+                      return ListView.builder(
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          NoteItem item = snapshot.data[index];
+                          return Dismissible(
+                            key: UniqueKey(),
+                            background: Container(color: Colors.red),
+                            onDismissed: (direction) {
+                              DBProvider.db.deleteClient(item.id);
+                              },
+                            //child: NoteItemView.viewTitle(item, this),
+                            child: NoteItemView(item, this),
+                          );
+                          },
+                      );
+                    } else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                    },
+                ),
+                Icon(Icons.access_alarm),
+                Icon(Icons.directions_bike),
+                Icon(Icons.directions_bike),
+              ],
+            ),
+//            floatingActionButton: FloatingActionButton(
+//              child: Icon(Icons.add),
+//              onPressed: () async {
+//                NoteItem rnd = testClients[math.Random().nextInt(testClients.length)];
+//                await DBProvider.db.newClient(rnd);
+//                setState(() {});
+//                },
+//            ),
+          ),
+        ),
     );
   }
 }
